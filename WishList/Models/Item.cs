c05832312e4e990f0338cef7d14c2e66f23097bb
Nemanja11 <47555562+Nemanja11@users.noreply.github.com>
2019-03-21 10:09:34 +0100

    aaa

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public String Description { get; set; }
+    }
+}
