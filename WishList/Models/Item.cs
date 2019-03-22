51a1ab20b8cf2644408b7398422fe81ce1d7fe62
nemanja11 <zindovicnemanja@gmail.com>
2019-03-22 10:37:13 +0100

    added changes

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -6,8 +6,9 @@ namespace WishList.Models
     public class Item
     {
         public int Id { get; set; }
+
         [Required]
         [MaxLength(50)]
-        public String Description { get; set; }
+      public String Description { get; set; }
     }
 }
